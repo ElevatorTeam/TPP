a8ebbb85bdd22bee17c718b95e038dace15c1f11
thisisdaleb <officialgsproductions@gmail.com>
2014-03-19 23:30:10 -0500

    ANNA DONT READ
    So, now the players flip back and forth depending on which way the other
    person is, and if you have 2  people of the same character, the second
    player is a darker shade. small reorginizations of code, and how player
    1 collides with the walls has been changed.

--- src/gameMode/Chars.java
+++ src/gameMode/Chars.java
@@ -14,14 +14,26 @@ public class Chars {
 	static SpriteSheet bird;
 	static SpriteSheet lazr;
 	static SpriteSheet atv;
+	static SpriteSheet dragFlipped;
+	static SpriteSheet flareonFlipped;
+	static SpriteSheet zapdFlipped;
+	static SpriteSheet birdFlipped;
+	static SpriteSheet lazrFlipped;
+	static SpriteSheet atvFlipped;
 	
 	//animations
 	public static Animation dragAnim;
+	public static Animation dragAnimFlipped;
 	public static Animation zapdAnim;
+	public static Animation zapdAnimFlipped;
 	public static Animation birdAnim;
+	public static Animation birdAnimFlipped;
 	public static Animation lazrAnim;
+	public static Animation lazrAnimFlipped;
 	public static Animation atvAnim;
+	public static Animation atvAnimFlipped;
 	public static Animation flareonAnim;
+	public static Animation flareonAnimFlipped;
 	
 	public static void createSpriteSheets(){
 		try {
@@ -31,32 +43,78 @@ public class Chars {
 			lazr = new SpriteSheet("resources/char/lazrgatrAnimation.png", 82, 98);
 			atv = new SpriteSheet("resources/char/atvAnimation.png", 123, 85);
 			flareon = new SpriteSheet("resources/char/falseProphetAnimation.png", 59, 93);
+			dragFlipped = new SpriteSheet("resources/char/dragoniteFlippedAnimation.png", 85, 98);
+			zapdFlipped = new SpriteSheet("resources/char/zapdosAnimation.png", 145, 106);
+			birdFlipped = new SpriteSheet("resources/char/birdJesusAnimation.png", 124, 124);
+			lazrFlipped = new SpriteSheet("resources/char/lazrgatrAnimation.png", 82, 98);
+			atvFlipped = new SpriteSheet("resources/char/atvAnimation.png", 123, 85);
+			flareonFlipped = new SpriteSheet("resources/char/falseProphetAnimation.png", 59, 93);
 		}catch (SlickException e){
 			e.printStackTrace();
 		}
 	}
 	
 	public static void createAnimations(){
+			dragAnimFlipped = new Animation(dragFlipped, 25);
 			dragAnim = new Animation(drag, 25);
-			zapdAnim = new Animation(zapd, 25);
-			birdAnim = new Animation(bird, 25);
-			lazrAnim = new Animation(lazr, 25);
-			atvAnim = new Animation(atv, 25);
-			flareonAnim = new Animation(flareon, 25);
+			
+			zapdAnimFlipped = new Animation(zapd, 25);
+			zapdAnim = new Animation(true);
+			for(int k=0; k<30; k++)
+				zapdAnim.addFrame(zapdAnimFlipped.getImage(k).getFlippedCopy(true, false), 25);
+			
+			birdAnimFlipped = new Animation(bird, 25);
+			birdAnim = new Animation(true);
+			for(int k=0; k<=52; k++)
+				birdAnim.addFrame(birdAnimFlipped.getImage(k).getFlippedCopy(true, false), 25);
+			
+			lazrAnimFlipped = new Animation(lazr, 25);
+			lazrAnim = new Animation(true);
+			for(int k=0; k<=63; k++)
+				lazrAnim.addFrame(lazrAnimFlipped.getImage(k).getFlippedCopy(true, false), 25);
+			
+			flareonAnimFlipped = new Animation(flareon, 25);
+			flareonAnim = new Animation(true);
+			for(int k=0; k<28; k++)
+				flareonAnim.addFrame(flareonAnimFlipped.getImage(k).getFlippedCopy(true, false), 25);
+			
+			atvAnimFlipped = new Animation(atv, 25);
+			atvAnim = new Animation(true);
+			for(int k=0; k<=47; k++)
+				atvAnim.addFrame(atvAnimFlipped.getImage(k).getFlippedCopy(true, false), 25);
+	}
+	
+	public static boolean checkDirection(int x1,int x2){
+		if(x1<x2){
+			return true;
+		}
+		return false;
 	}
 	
 	public static void restartAnimations(){
 		if(dragAnim.getFrame()>=43)
 			dragAnim.restart();
+		if(dragAnimFlipped.getFrame()>=43)
+			dragAnimFlipped.restart();
 		if(zapdAnim.getFrame()>=29)
 			zapdAnim.restart();
+		if(zapdAnimFlipped.getFrame()>=29)
+			zapdAnimFlipped.restart();
 		if(birdAnim.getFrame()>=52)
 			birdAnim.restart();
+		if(birdAnimFlipped.getFrame()>=52)
+			birdAnimFlipped.restart();
 		if(lazrAnim.getFrame()>=63)
 			lazrAnim.restart();
+		if(lazrAnimFlipped.getFrame()>=63)
+			lazrAnimFlipped.restart();
 		if(flareonAnim.getFrame()>=28)
 			flareonAnim.restart();
+		if(flareonAnimFlipped.getFrame()>=28)
+			flareonAnimFlipped.restart();
 		if(atvAnim.getFrame()>=47)
 			atvAnim.restart();
+		if(atvAnimFlipped.getFrame()>=47)
+			atvAnimFlipped.restart();
 	}
 }
